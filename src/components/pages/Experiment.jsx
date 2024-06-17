64d965b27b2ae806a02b3c9c512e5fc854a7db66
Artur Valiullin <ar.valiullin@yandex.ru>
2024-06-17 20:25:09 +0300

    Experiment.jsx: add Save to csv

--- src/components/pages/Experiment.jsx
+++ src/components/pages/Experiment.jsx
@@ -1,43 +1,70 @@
-import {useEffect, useState} from 'react';
-import {useParams} from 'react-router-dom';
-import PropTypes from 'prop-types';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import PropTypes from "prop-types";
 
 import PerformanceTable from "../table/PerfomanceTable.jsx";
 import ChartComponent from "../charts/ChartComponent.jsx";
 
-function Experiment({performanceExperiments}) {
+function Experiment({ performanceExperiments }) {
+  const { experimentId } = useParams();
+  const [tableData, setTableData] = useState([]);
+  const [stateMessage, setStateMessage] = useState("Начать эксперимент");
 
-    const {experimentId} = useParams();
-    const [tableData, setTableData] = useState([]);
-    const [stateMessage, setStateMessage] = useState("Начать эксперимент");
+  const runPerformanceExperiment = async (experimentId) => {
+    const experiment = performanceExperiments[experimentId];
+    return experiment.calculatePerformance();
+  };
 
-    const runPerformanceExperiment = async (experimentId) => {
-        const experiment = performanceExperiments[experimentId];
-        return experiment.calculatePerformance();
-    };
+  async function handleRecalculate() {
+    setStateMessage("Идет эксперимент");
+    runPerformanceExperiment(experimentId)
+      .then((performanceValues) => {
+        setTableData(performanceValues);
+        setStateMessage("Начать эксперимент");
+      })
+      .catch(() => {
+        setStateMessage("Ошибка");
+      });
+  }
 
-    async function handleRecalculate() {
-        setStateMessage("Идет эксперимент");
-        runPerformanceExperiment(experimentId).then((performanceValues) => {
-            setTableData(performanceValues);
-            setStateMessage("Начать эксперимент");
-        }).catch(() => {
-            setStateMessage("Ошибка");
-        });
-    }
+  function exportToCSV() {
+    const csvContent =
+      "N, JavaScript, WebAssembly, Acceleration\n" +
+      tableData
+        .map(
+          (data) =>
+            `${data.N},${data.JavaScript},${data.WebAssembly},${
+              data.JavaScript / data.WebAssembly
+            }`
+        )
+        .join("\n");
+    const blob = new Blob([csvContent], { type: "text/csv" });
+    const url = URL.createObjectURL(blob);
 
-    return (<>
-        <h1>Эксперимент {experimentId}</h1>
-        <button onClick={handleRecalculate}> {stateMessage}</button>
-        <div className="card">
-            <PerformanceTable key="performanceTable" tableData={tableData}/>
-            <ChartComponent key="chartComponent" tableData={tableData}/>
-        </div>
-    </>);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = `${experimentId}_data.csv`;
+    a.click();
+
+    URL.revokeObjectURL(url);
+  }
+
+  return (
+    <>
+      <h1>Эксперимент {experimentId}</h1>
+      <button onClick={handleRecalculate}>{stateMessage}</button>
+      <span style={{ margin: "0 10px" }}></span>
+      <button onClick={exportToCSV}>Сохранить в CSV</button>
+      <div className="card">
+        <PerformanceTable key="performanceTable" tableData={tableData} />
+        <ChartComponent key="chartComponent" tableData={tableData} />
+      </div>
+    </>
+  );
 }
 
 Experiment.propTypes = {
-    performanceExperiments: PropTypes.object.isRequired,
+  performanceExperiments: PropTypes.object.isRequired,
 };
 
 export default Experiment;
